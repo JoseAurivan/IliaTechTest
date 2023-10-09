7e6dd8df3992ccb09228f08917424d793fd7ef91
jose.aurivan <aurivanfilho2013@gmail.com>
2023-10-09 15:30:24 -0300

    customer components and page

--- src/app/order/page.tsx
+++ src/app/order/page.tsx
@@ -1,8 +1,35 @@
+import CustomerList from '@/components/CustomerList';
+import style from './Order.module.scss'
+import { useState } from "react"
+
 export default function Order()
 {
+    const [customerName, setCustomerName] = useState('');
+    const [customerEmail, setCustomerEmail] = useState('');
+
+    function saveCustomer()
+    {
+        const customer = {customerName,customerEmail};
+        //dispatch to Redux
+    }
+    function finishAllCustomers()
+    {
+
+    }
+
     return(
         <div>
-            ORDERS
+            <form>
+                <label>Name:</label>
+                <input type="text" id="name" ></input>
+                <label>Email:</label>
+                <input type="email"></input>
+                <button type="submit" onClick={saveCustomer}></button>
+            </form>
+            <CustomerList/>
+            <div>
+                <button onClick={finishAllCustomers}>Close night</button>
+            </div>
         </div>
     )
 }
