a25a651b154ad2c84cf81efd12255386e5743691
jose.aurivan <aurivanfilho2013@gmail.com>
2023-10-10 20:35:31 -0300

    home page stylization complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
 import Navbar from '@/components/Navbar'
 import style from './Home.module.scss'
+import classNames from 'classnames'
 export default function Home() {
   return (
-    <div id="main" className={style.text}>
-      <h1>Hello world !</h1>
+    <div className={style.container}>
+      <div className={classNames({'card':true},{[style.home]:true})}>        
+        <h5 className="card-header">Home Page</h5>
+          <div className="card-body">
+            <h6 className="card-subtitle mb-2 text-muted">This is the home page of the project</h6>
+            <p  className="card-text">This project consumes a public api and uses Redux to manage state.</p>
+            <p className="card-text"> You can check the api fetch working on the API link.</p>
+            <p className="card-text">You can check the Redux working on the Customer link</p>
+        </div>
+      </div>
     </div>
   )
 }
