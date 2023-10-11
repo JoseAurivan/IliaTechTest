909b539c5cc46c4dbff69da9ecd4066da6e35b37
jose.aurivan <aurivanfilho2013@gmail.com>
2023-10-11 12:03:29 -0300

    name size validaton done

--- src/__tests__/Validations/Validatios.test.ts
+++ src/__tests__/Validations/Validatios.test.ts
@@ -1,4 +1,4 @@
-import { validEmail} from '@/validation/validate'
+import { validEmail, validNameSize} from '@/validation/validate'
 
 describe('Email Validation',()=>{
     it("Expect success when  email is  valid ",()=>{
@@ -13,5 +13,16 @@ describe('Email Validation',()=>{
 })
 
 describe("Name size validaion",()=>{
-    
+    it("Expect sucess when name has minimun size of 1",()=>{
+        const name="a";
+        expect(validNameSize.test(name)).toBe(true);
+    })
+    it("Expect error when name does not have minimun size of 1",()=>{
+        const name="";
+        expect(validNameSize.test(name)).toBe(false);
+    })
+    it("Expect sucess when name has size bigger than 1",()=>{
+        const name="Solid Snake";
+        expect(validNameSize.test(name)).toBe(true);
+    })
 })
