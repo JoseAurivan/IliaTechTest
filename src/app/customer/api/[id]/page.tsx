10e67a9a74e8c86b9568e8de3a20e40f5e6f37d0
jose.aurivan <aurivanfilho2013@gmail.com>
2023-10-12 14:42:33 -0300

    delete orders and clients

--- src/app/customer/api/[id]/page.tsx
+++ src/app/customer/api/[id]/page.tsx
@@ -3,7 +3,8 @@
 import { IOrder } from "@/types/order";
 import { fetchOrdersFromCustomer } from "@/utils/api";
 import { useState, useEffect } from "react";
-
+import { deleteOrder } from "@/utils/api";
+import { useRouter } from "next/navigation";
 
 
 export default function ApiCustomer({params}:{params:{id:number}}){
@@ -11,13 +12,15 @@ export default function ApiCustomer({params}:{params:{id:number}}){
     const [data, setData] = useState<IOrder[]>([]);
     const [message, setMessage] = useState("Getting data from backend...");
 
+    const router = useRouter();
+
     useEffect(()=>{
       const fetchDataAsync = async () => {
       try {
         const result = await fetchOrdersFromCustomer(params.id);
         setData(result);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        setMessage("Problem with back end");
       }
     };
 
@@ -28,13 +31,22 @@ export default function ApiCustomer({params}:{params:{id:number}}){
   
     },[data.length,params.id])
 
+    async function HandleDelete(id: number){
+      const response = await deleteOrder(id);
+      console.log(response.toString());
+      window.location.reload();
+    }
+
     return(
-        <div>
+        <div className="container">
+          <button type="button" onClick={()=>router.push('/customer/api')} className="btn btn-outline-warning mt-2 mb-2">Back</button>
+          <ol className="list-group list-group-numbered">
             {data.length > 0 ?
              data.map(data =>(
-              <li key={data.orderId}> {data.description} </li>
+              <li key={data.orderId} className="p-3 border bg-light rounded"> {data.description} <button className="btn btn-danger" onClick={()=>HandleDelete(data.orderId)}>DELETE</button> </li>
              )) 
              : <p> {message}</p>}
+          </ol>
         </div>
     )
 }
